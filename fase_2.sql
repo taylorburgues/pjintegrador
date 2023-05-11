6802de3b824e2c6343fd88d4314237ba42d282e3
Taylor Matheus <taylorburgues>
2023-05-11 16:24:02 -0300

    bd

---
+++ fase_2.sql
@@ -0,0 +1,12 @@
+create table Amostras (
+  MP10 int(10) NOT NULL,
+  MP25 int(10) NOT NULL,
+  O3 int(10) NOT NULL,
+  CO int(10) NOT NULL,
+  NO2 int(10) NOT NULL,
+  SO2 int(10) NOT NULL
+) DEFAULT CHARSET=utf8;
+
+select * from amostras;
+
+#coloquei no vscode apenas para subir tudo junto
